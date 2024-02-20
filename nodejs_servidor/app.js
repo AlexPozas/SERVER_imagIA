576ea3ae8baf117348ced585e7b95399fa29fe5d
Yuheng30 <yzhou.cf@iesesteveterradas.cat>
2024-02-20 20:44:36 +0100

    v2

--- nodejs_servidor/app.js
+++ nodejs_servidor/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const multer = require('multer');
 const url = require('url')
 const fs = require('fs');
+const axios = require('axios');
 
 const app = express()
 const port = process.env.PORT || 80
@@ -56,44 +57,24 @@ app.post('/data', upload.single('file'), async (req, res) => {
         prompt: messageText,
         images: [imageList],
       };
+      const response = await axios.post(apiUrl, requestData);
 
-      fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-      }).then(function (respuesta) {
-        if (!respuesta.ok) {
-          res.status(400).send('Error en la solicitud.')
-          throw new Error("Error en la solicitud");
+      const responses = [];
+      response.data.split('\n').forEach(line => {
+        if (line.trim() !== '') {
+          const responseObj = JSON.parse(line);
+          responses.push(responseObj);
         }
-        return respuesta.text();
-      })
-        .then(function (datosRespuesta) {
-          var lineas = datosRespuesta.split('\n');
+      });
 
-          var objetosJSON = [];
-          for (var i = 0; i < lineas.length; i++) {
-            var linea = lineas[i].trim();
-            if (linea) {
-              objetosJSON.push(JSON.parse(linea));
-            }
-          }
-
-          res.writeHead(200, { 'Content-Type': 'text/plain; charset=UTF-8' })
-          var resp = "";
-          objetosJSON.forEach(function (objeto) {
-            resp = resp + objeto.response;
-            res.write(objeto.response);
-          });
-
-          console.log('image response');
-          res.end("")
-        })
-        .catch(function (error) {
-          console.error("Error en la solicitud:", error);
-        });
+      const jsonResponse = {
+        type: 'respuesta',
+        mensaje: responses.map(response => response.response).join(''),
+      };
+  
+      console.log(jsonResponse);
+      res.status(200).json(jsonResponse);
+      responses.clear;
 
 
     } catch (error) {

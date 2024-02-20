a7b96f8f6a9fdf6c9be2659fee96d19203d948cb
Yuheng30 <yzhou.cf@iesesteveterradas.cat>
2024-02-20 21:08:00 +0100

    v2

--- nodejs_servidor/app.js
+++ nodejs_servidor/app.js
@@ -1,50 +1,46 @@
-const express = require('express')
+const express = require('express');
 const multer = require('multer');
-const url = require('url')
-const fs = require('fs');
+const axios = require('axios');
 
-const app = express()
-const port = process.env.PORT || 80
+const app = express();
+const port = process.env.PORT || 80;
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(express.json());
 
 const httpServer = app.listen(port, async () => {
-  console.log(`Listening for HTTP queries on: http://0.0.0.0:${port}`)
-})
+  console.log(`Listening for HTTP queries on: http://0.0.0.0:${port}`);
+});
 
 process.on('SIGTERM', shutDown);
 process.on('SIGINT', shutDown);
 
 function shutDown() {
   console.log('Received kill signal, shutting down gracefully');
-  httpServer.close()
+  httpServer.close();
   process.exit(0);
 }
 
 app.post('/data', upload.single('file'), async (req, res) => {
   const textPost = req.body;
   const uploadedFile = req.file;
-  let objPost = {}
+  let objPost = {};
   console.log(textPost);
   try {
-    objPost = textPost.data
+    objPost = textPost.data;
   } catch (error) {
-    res.status(400).send('Sol·licitud incorrecta.')
-    console.log(error)
-    return
+    res.status(400).send('Solicitud incorrecta.');
+    console.log(error);
+    return;
   }
 
   if (objPost.type == 'image') {
     try {
       const messageText = objPost.prompt;
-
-     // console.log(objPost.image);
       const imageList = objPost.image;
-
 
       const url = 'http://192.168.1.14:11434/api/generate';
       const data = {
@@ -53,51 +49,33 @@ app.post('/data', upload.single('file'), async (req, res) => {
         images: [imageList]
       };
 
-      fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-      }).then(function (respuesta) {
-        if (!respuesta.ok) {
-          res.status(400).send('Error en la solicitud.')
-          throw new Error("Error en la solicitud");
-        }
-        return respuesta.text();
-      })
-        .then(function (datosRespuesta) {
-          var lineas = datosRespuesta.split('\n');
+      axios.post(url, data)
+        .then(function (response) {
+          const responseData = response.data;
+          const responseLines = responseData.split('\n');
+          let responseBody = '';
 
-          var objetosJSON = [];
-          for (var i = 0; i < lineas.length; i++) {
-            var linea = lineas[i].trim();
-            if (linea) {
-              objetosJSON.push(JSON.parse(linea));
+          for (const line of responseLines) {
+            if (line.trim() !== '') {
+              const responseObject = JSON.parse(line);
+              responseBody += responseObject.response;
             }
           }
 
-          res.writeHead(200, { 'Content-Type': 'text/plain; charset=UTF-8' })
-          var resp = "";
-          objetosJSON.forEach(function (objeto) {
-            resp = resp + objeto.response;
-            res.write(objeto.response);
-          });
-
-          console.log('image response');
-          res.end("")
+          console.log('image response:', responseBody);
+          res.status(200).send(responseBody);
         })
         .catch(function (error) {
           console.error("Error en la solicitud:", error);
+          res.status(500).send('Error procesando la solicitud.');
         });
-
 
     } catch (error) {
       console.log(error);
-      res.status(500).send('Error processing request.');
+      res.status(500).send('Error procesando la solicitud.');
     }
   } else {
-    console.log('error, type not exists')
-    res.status(400).send('Sol·licitud incorrecta.')
+    console.log('error, type not exists');
+    res.status(400).send('Solicitud incorrecta.');
   }
-})
+});

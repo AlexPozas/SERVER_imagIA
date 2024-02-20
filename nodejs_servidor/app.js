8857203fa064d6499fa059425d3b4fe078af5b4d
Yuheng30 <yzhou.cf@iesesteveterradas.cat>
2024-02-20 20:36:11 +0100

    v2

--- nodejs_servidor/app.js
+++ nodejs_servidor/app.js
@@ -52,47 +52,34 @@ app.post('/data', upload.single('file'), async (req, res) => {
 
       let url = 'http://192.168.1.14:11434/api/maria/image';
       var data = {
+        model: 'llava',
         prompt: messageText,
-        images: imageList
+        images: [imageList],
       };
 
-      fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-      }).then(function (respuesta) {
-        if (!respuesta.ok) {
-          res.status(400).send('Error en la solicitud.')
-          throw new Error("Error en la solicitud");
-        }
-        return respuesta.text();
-      })
-        .then(function (datosRespuesta) {
-          var lineas = datosRespuesta.split('\n');
+      const response = await axios.post(apiUrl, requestData);
 
-          var objetosJSON = [];
-          for (var i = 0; i < lineas.length; i++) {
-            var linea = lineas[i].trim();
-            if (linea) {
-              objetosJSON.push(JSON.parse(linea));
-            }
+        const responses = [];
+        response.data.split('\n').forEach(line => {
+          if (line.trim() !== '') {
+            const responseObj = JSON.parse(line);
+            responses.push(responseObj);
           }
-
-          res.writeHead(200, { 'Content-Type': 'text/plain; charset=UTF-8' })
-          var resp = "";
-          objetosJSON.forEach(function (objeto) {
-            resp = resp + objeto.response;
-            res.write(objeto.response);
-          });
-
-          console.log('image response');
-          res.end("")
-        })
-        .catch(function (error) {
-          console.error("Error en la solicitud:", error);
         });
+
+        // Construir un objeto JSON con la estructura deseada
+        const jsonResponse = {
+          type: 'respuesta',
+          mensaje: responses.map(response => response.response).join(''),
+        };
+       /*{
+          type: 'respuesta',
+          mensaje: " In this picture, there is a large elephant with a wide trunk standing in the grass. It appears to be an adult male African elephant. The setting is outdoors with the sky visible above the elephant's head, creating a serene and natural atmosphere."
+        }*/
+       
+        // Enviar la respuesta JSON al cliente
+        res.status(200).json(jsonResponse);
+        responses.clear;
 
 
     } catch (error) {

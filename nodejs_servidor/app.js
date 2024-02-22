9ab5db28217d588ac740375117d10edfb55daa50
Yuheng30 <yzhou.cf@iesesteveterradas.cat>
2024-02-22 15:27:26 +0100

    sprint 1

--- nodejs_servidor/app.js
+++ nodejs_servidor/app.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 app.use(express.json());
 
 const httpServer = app.listen(port, async () => {
-  console.log(`Listening for HTTP queries on: http://0.0.0.0:${port}`);
+  console.log(`Listening for HTTP queries on: http://192.168.151.167:${port}`);
 });
 
 process.on('SIGTERM', shutDown);
